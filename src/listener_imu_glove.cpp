4cd1aa2d141221ce934002c16282c73f904ec37f
emalbt <emanuele.luberto@gmail.com>
2016-11-28 10:33:29 +0100

    [EL] <kbhit> keyboard input in main.cpp

--- src/listener_imu_glove.cpp
+++ src/listener_imu_glove.cpp
@@ -22,34 +22,6 @@ imu_glove::~imu_glove()
 
 /*********************************************************************/
 
-int imu_glove::kbhit(void) {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
-}
-
-/*********************************************************************/
-
 void imu_glove::openFiles()
 {
   std::string s, s1, s2;
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,49 @@
+#include <ros/ros.h>
+#include <boost/asio.hpp>
+
+// my headers
 // #include "listener_phase_space.h"
 #include "listener_imu_glove.h"
 
-#include <ros/ros.h>
+
+
+//-----------------------------------------------------
+//                                             	  kbhit
+//-----------------------------------------------------
+int kbhit(void) 
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
 
 
 
 
 
+//-----------------------------------------------------
+//                                             	   main
+//-----------------------------------------------------
 int main(int argc, char **argv)
 {
 	char x;
@@ -22,7 +59,7 @@ int main(int argc, char **argv)
 	while(ros::ok())
 	{
 
-		if (IG.kbhit())
+		if (kbhit())
 		{
         	x = getchar();
         	if(x == 'o') 
